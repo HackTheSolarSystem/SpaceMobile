694b1484de4c672bed5ce5b315d9df08d44559eb
Harry C <harrychenhc@icloud.com>
2019-02-09 22:50:23 -0500

    added transition to game view

--- SpaceApp/Navigation/index.js
+++ SpaceApp/Navigation/index.js
@@ -2,9 +2,11 @@ import { createAppContainer, createStackNavigator } from 'react-navigation'
 
 import GameMenu from '../Views/GameMenu'
 import Test from '../Views/Test'
+import AugmentedGame from '../Views/AugmentedGames'
 
 const AppNavigator = createStackNavigator({
   GameMenu,
+  AugmentedGame,
   Test
 }, {
   initialRouteName: 'GameMenu'
--- SpaceApp/Views/GameMenu.js
+++ SpaceApp/Views/GameMenu.js
@@ -1,18 +1,34 @@
-import React from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import React, { Fragment } from 'react'
+import { View, Text, TouchableOpacity, Button, StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    justifyContent: 'center',
+    paddingTop: 20
+  }
+});
 
 class GameMenu extends React.Component{
   render(){
     const { navigation } = this.props
     return (
-      <View>
-        <Text>Augmented Game</Text>
-        <TouchableOpacity onPress={() => navigation.navigate('AugmentedGame')}>
-          <Text>Start</Text>
-        </TouchableOpacity>
-      </View>
+      <Fragment>
+        <View>
+          <Text style={styles.title}>Augmented Game</Text>
+        </View>
+        <View style={styles.container}>
+          <TouchableOpacity onPress={() => navigation.navigate('AugmentedGame')}>
+            <Text style={{alignItems: 'center'}}>Start</Text>
+          </TouchableOpacity>
+        </View>
+      </Fragment>
     )
-
   }
 }
 
