01570f0369ba310acee031aa2724ed882351801d
Harry C <harrychenhc@icloud.com>
2019-02-09 22:23:04 -0500

    fixed variable prob

--- SpaceApp/Views/GameMenu.js
+++ SpaceApp/Views/GameMenu.js
@@ -1,19 +1,19 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 
 class GameMenu extends React.Component{
-
-
   render(){
+    const { navigation } = this.props
     return (
       <View>
         <Text>Augmented Game</Text>
+        <TouchableOpacity onPress={() => navigation.navigate('AugmentedGame')}>
+          <Text>Start</Text>
+        </TouchableOpacity>
       </View>
-
-
     )
 
   }
 }
 
-export default AugmentedGame
+export default GameMenu
