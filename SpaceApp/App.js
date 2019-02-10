58d9ac24c58faa81e7020a77d5724f5605ea876c
Harry C <harrychenhc@icloud.com>
2019-02-09 23:56:24 -0500

    added buggy code

--- SpaceApp/App.js
+++ SpaceApp/App.js
@@ -6,9 +6,6 @@ import AppContainer from './Navigation'
 export default class App extends React.Component {
   render() {
     return (
-      // <View style={styles.container}>
-      //   <Text>Open up App.js to start working on your app!</Text>
-      // </View>
       <AppContainer/>
     );
   }
