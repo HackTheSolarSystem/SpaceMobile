5d198890c4909374f1ee0333fb5b3c70fd8de137
Harry C <harrychenhc@icloud.com>
2019-02-10 02:46:38 -0500

    added redux store

---
+++ SpaceApp/store/index.js
@@ -0,0 +1,12 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+
+import playerReducer from './reducers/player'
+import teammatesReducer from './reducers/teammates'
+
+const reducer = combineReducers({
+  player: playerReducer,
+  teammates: teammatesReducer
+})
+
+const store = createStore(reducer, applyMiddleware(thunk))
